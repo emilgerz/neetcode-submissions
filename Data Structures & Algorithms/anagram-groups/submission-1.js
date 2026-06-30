709dee5e1b6f6fef7714f7e16469a312bdd51494
Emil Gerz <c0nr33d@gmail.com>
2026-06-30 14:05:19 +0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const anagrams = {}
+
+        const ANAGRAM_LENGTH = 'z'.codePointAt() - 'a'.codePointAt()
+        let currentAnagram = Array(ANAGRAM_LENGTH).fill(0)
+
+        for (const word of strs) {
+            for (let i = 0; i < word.length; i++) {
+                currentAnagram[word[i].codePointAt() - 'a'.codePointAt()]++
+            }
+
+            const anagramKey = currentAnagram.join(',')
+
+            anagrams[anagramKey] ??= []
+            anagrams[anagramKey].push(word)
+
+            currentAnagram = Array(ANAGRAM_LENGTH).fill(0)
+        }
+
+        return Object.values(anagrams)
+    }
+}
