11e9cd6c1f5238ded6a7815f9078750a613e5114
Emil Gerz <c0nr33d@gmail.com>
2026-06-30 18:37:18 +0300

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {return JSON.stringify(strs)}
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {return JSON.parse(str)}
+}
