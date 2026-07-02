e967c14c50c4268efed10c538cd0c653f1d6e440
Emil Gerz <c0nr33d@gmail.com>
2026-07-02 16:33:06 +0300

    Add: lowest-common-ancestor-in-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-0.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {TreeNode}
+     */
+    lowestCommonAncestor(root, p, q) {
+        const findPath = (node, requested, path = []) => {
+            if (node === null) {
+                return null
+            }
+
+            const newPath = path.concat(node)
+
+            if (node.val === requested.val) {
+                return newPath
+            }
+
+            return findPath(node.left, requested, newPath) ?? findPath(node.right, requested, newPath) 
+        }
+
+        const pPath = findPath(root, p)
+
+        const qPath = findPath(root, q)
+
+        const minLength = Math.min(pPath.length, qPath.length)
+
+        for (let i = 0; i < minLength; i++) {
+            if (pPath[i + 1]?.val !== qPath[i + 1]?.val) {
+                return pPath[i]
+            }
+        }
+
+        return null
+    }
+}
