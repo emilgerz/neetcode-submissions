2d7f2e137f164e385265dcc3aaf4ff5a98cabfec
Emil Gerz <c0nr33d@gmail.com>
2026-07-17 15:11:38 +0300

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        const operands = []
+
+        const evaluate = {
+            "+": (a, b) => a + b,
+            "-": (a, b) => a - b,
+            "*": (a, b) => a * b,
+            "/": (a, b) => a / b 
+        }
+
+        const OPERATORS = new Set(Object.getOwnPropertyNames(evaluate))
+
+        for (const symb of tokens) {
+            console.log(operands)
+            if (OPERATORS.has(symb)) {
+                const second = operands.pop()
+                const first = operands.pop()
+
+                const res = evaluate[symb](first, second)
+
+                operands.push(Math.trunc(res))
+            } else {
+                operands.push(Number(symb))
+            }
+        }
+
+        return operands[0]
+    }
+}
