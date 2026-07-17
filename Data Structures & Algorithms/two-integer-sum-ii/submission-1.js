4a1df70e679cfe76a10308ad06926f97b2c76d72
Emil Gerz <c0nr33d@gmail.com>
2026-07-17 12:22:53 +0300

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        const diff_to_idx = new Map()
+
+        for (let i = 0; i < numbers.length; i++) {
+            if (diff_to_idx.has(numbers[i])) {
+                return [diff_to_idx.get(numbers[i]) + 1, i + 1]
+            }
+
+            const wanted = target - numbers[i]
+
+            diff_to_idx.set(wanted, i)
+        }
+
+        return -1
+    }
+}
